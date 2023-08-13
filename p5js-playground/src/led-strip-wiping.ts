21196ae0c01c3f2804812d174cb7340312988e41
botamochi6277 <botamochi6277@icloud.com>
2023-08-13 15:51:00 +0900

    add direction radio button

--- p5js-playground/src/led-strip-wiping.ts
+++ p5js-playground/src/led-strip-wiping.ts
@@ -50,8 +50,15 @@ const sketch = (p5: p5) => {
     }
   }
 
+  type RadioButton = p5.Element & {
+    option: (name: string) => null,
+    selected: (name: string) => null
+  }
+
   let progress_slider: p5.Element;
   let window_width: number = window.innerWidth;
+  let direction_radio: RadioButton;
+  let transition_radio: RadioButton;
 
 
   const aspect_ratio = 0.25;
@@ -72,6 +79,21 @@ const sketch = (p5: p5) => {
     canvas.parent('canvas-led-strip');
     p5.colorMode(p5.HSB, 100, 100, 100);
     progress_slider = p5.createSlider(0, 1.0, 0.5, 1 / (num_pixels + 1));
+
+
+    // setup direction radio button
+    direction_radio = p5.createRadio("direction radio") as RadioButton;
+    direction_radio.option("forward");
+    direction_radio.option("backward");
+    direction_radio.selected("forward");
+
+    // setup transition radio button
+    transition_radio = p5.createRadio("transition_radio") as RadioButton;
+    transition_radio.option("wipe");
+    transition_radio.option("dissolve");
+    transition_radio.option("slide");
+    transition_radio.selected("wipe");
+
     onWindowResized();// init sizing
   };
 
@@ -84,11 +106,11 @@ const sketch = (p5: p5) => {
     const progress_ratio = Number(progress_slider.value());
     const weight = Array(num_pixels).fill(0.0);
 
-    const backward = false;
+    const is_backward = direction_radio.value() === "backward";
     const blur_width = 4 / num_pixels;
     for (let index = 0; index < num_pixels; index++) {
       const r = index / num_pixels;
-      if (!backward) {
+      if (!is_backward) {
         weight[index] = hardSigmoid((1.0 - r) + (1.0 + blur_width) * progress_ratio - 1.0, blur_width);
       } else {
         weight[index] = hardSigmoid(r + (1.0 + blur_width) * progress_ratio - 1.0, blur_width);
